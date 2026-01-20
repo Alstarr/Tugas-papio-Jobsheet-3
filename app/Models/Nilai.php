265804087be0b63e1a6cc85271f1de1a24fd493d
Alstarr <farisi3012@gmail.com>
2026-01-20 21:07:49 +0700

    jobsheet 3

---
+++ app/Models/Nilai.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nilai extends Model
+{
+    protected $table = 'nilai';
+
+    protected $fillable = [
+        'siswa_id',
+        'mata_pelajaran',
+        'skor'
+    ];
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, 'siswa_id');
+    }
+}
---
+++ app/Models/Siswa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $table = 'siswa';
+
+    protected $fillable = [
+        'nisn',
+        'nama_siswa',
+        'jenis_kelamin',
+        'kelas_id'
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'kelas_id');
+    }
+
+    public function nilai()
+    {
+        return $this->hasMany(Nilai::class, 'siswa_id');
+    }
+}
